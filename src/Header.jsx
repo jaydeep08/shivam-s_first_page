60fc4db193c83d3b978d346ccaab41d676c61ccd
jaydeep08 <jaydeeppatidar008@gmail.com>
2022-05-16 23:32:15 +0530

    Initial commit

---
+++ src/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './header.css';
+
+function Header(){
+    return <>
+
+<h1>jaydeep</h1>
+
+<h2 class="shiv">Card</h2>
+
+<div class="card">
+  <img src="img_avatar.png" alt="Avatar"  />
+  <div class="container">
+    <h4><b>John Doe</b></h4> 
+    <p>Architect & Engineer</p> 
+  </div>
+</div>
+    </>
+}
+export default Header;
