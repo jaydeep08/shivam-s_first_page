7578ceff88952f10ede59adf249b18a232d3ea2a
jaydeep08 <jaydeeppatidar008@gmail.com>
2022-05-17 12:29:08 +0530

    Initial commit

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,18 +1,33 @@
 import React from 'react';
 import './header.css';
+import { useState, useEffect } from "react";
 
 function Header(){
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+  fetch(`http://api.weatherapi.com/v1/current.json?key=042ca2d4adbf47b08a153518221705&q=India&aqi=no`)
+   .then((response) => response.json()).then((daata)=>{
+       setData(daata);
+   });
+ }, []);
+console.log(data);
     return <>
 
-<h1>jaydeep</h1>
+<h1 style={{color:"rgb(146, 93, 15)",backgroundColor:"lightgreen",textAlign:"center"}}>Weather Forcast</h1>
 
-<h2 class="shiv">Card</h2>
 
-<div class="card">
-  <img src="img_avatar.png" alt="Avatar"  />
+
+<div className="card container">
+  <img style={{width:"100px",height:"100px"}} src="https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather02-1024.png" alt="Avatar"  />
   <div class="container">
-    <h4><b>John Doe</b></h4> 
-    <p>Architect & Engineer</p> 
+  <h4><b>country: {data.location.country}</b></h4> 
+  <h4><b>{data.location.name}</b></h4> 
+  <p>Date: {data.current.last_updated}</p> 
+  <p>Humidity: {data.current.humidity}</p> 
+  <p>Temperature: {data.current.temp_c} Celsius &#47; {data.current.temp_f} Fahrenheit </p> 
+  <p>Wind: {data.current.wind_kph} kph</p> 
+ 
   </div>
 </div>
     </>
